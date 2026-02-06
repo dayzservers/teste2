d34776f68dbad9225620cf9556ab2e485da9a89a
dayzservers <dayz.servers@outlook.com>
2026-02-06 09:23:59 -0300

    Update app.py

--- app.py
+++ app.py
@@ -54,6 +54,5 @@ def proxy_ttyd(path):
                     status=resp.status_code,
                     headers=headers)
 
-
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    app.run(host="0.0.0.0", port=5000, threaded=True)
